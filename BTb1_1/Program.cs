81e4b47c0f7f5c154bf904589937f7edd5e5bd52
Trần Minh Tú <6551071093@st.utc2.edu.vn>
2026-08-27 17:11:21 +0700

    Add files via upload

---
+++ BTb1_1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace BTb1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSSV: 6551071093\n");
+           
+            Console.Write("Ho ten: ");
+            string hoten = Console.ReadLine();
+       
+            Console.WriteLine();
+            Console.Write("Tuoi: ");
+            int tuoi = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Chieu cao: ");
+            decimal cao = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Can nang: ");
+            int canNang = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Da tot nghiep(true/false): ");
+            bool daTotNghiep = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Ho ten:    \t"+hoten+"\n");
+            Console.WriteLine("Tuoi:      \t"+tuoi+" tuoi\n");
+            Console.WriteLine("Chieu cao: \t"+cao+" m\n");
+            Console.WriteLine("Can nang:  \t"+canNang+" kg\n");
+            Console.WriteLine("Da tot nghiep:  "+ daTotNghiep);
+            Console.WriteLine();
+        }
+    }
+}
